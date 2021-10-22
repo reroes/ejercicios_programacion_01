18393f28d1556d48499e6e4b0d96e427f316804a
@reroes <reroes799@gmail.com>
2021-10-22 10:54:40 -0500

    mi 2do commit

---
+++ proyecto01_demo/src/proyecto01/Proyecto01.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+Generando un miniprograma en Java 
+Usando Netbeans
+junto
+Git y GitHub
+ */
+package proyecto01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Generando un miniprograma en Java");
+        System.out.println("Usando Netbeans");
+        System.out.println("junto");
+        System.out.println("Git y GitHub");
+        
+    }
+    
+}
